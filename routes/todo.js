dff5957522b417b190cbc1076a2b792eb2282c13
Enzo <enzosandoval@outlook.com>
2020-11-13 02:25:15 -0300

    f
    f

--- routes/todo.js
+++ routes/todo.js
@@ -39,36 +39,35 @@ class Todo {
             }
         });
     }
-}
 
-//Perfect, retorna un json
-newTicket(req, res) {
-    let item = new Ticket({
-        estado: req.body.estado
-    });
-    item.save(err => {
-        if (err) return res.status(500).send(err);
-        return res.status(200).send(item);
-    });
-}
+    //Perfect, retorna un json
+    newTicket(req, res) {
+        let item = new Ticket({
+            estado: req.body.estado
+        });
+        item.save(err => {
+            if (err) return res.status(500).send(err);
+            return res.status(200).send(item);
+        });
+    }
 
-/*
-{
-    "id":"5fa643dfec3fa12d44bc7faa",
-    "estado": true
-}
-*/
-updateTicket(req, res) {
-    Ticket.updateOne({ _id: req.body.id }, { estado: req.body.new_status }).then(doc => {
-        res.json({
-            ok: true
+    /*
+    {
+        "id":"5fa643dfec3fa12d44bc7faa",
+        "estado": true
+    }
+    */
+    updateTicket(req, res) {
+        Ticket.updateOne({ _id: req.body.id }, { estado: req.body.new_status }).then(doc => {
+            res.json({
+                ok: true
+            })
+        }, err => {
+            res.status(500).json({
+                error: 'Error al modificar ticket ' + req.body.id
+            })
         })
-    }, err => {
-        res.status(500).json({
-            error: 'Error al modificar ticket ' + req.body.id
-        })
-    })
-}
+    }
 }
 
 module.exports = new Todo().app;
